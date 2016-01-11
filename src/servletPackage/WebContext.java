ce153c4251197b1e4bc0a3034bead9616e838919
ahmetshen-us <ahmetshen.us@gmail.com>
2016-01-10 22:06:32 -0500

    challenge access and modify a web context

--- src/servletPackage/WebContext.java
+++ src/servletPackage/WebContext.java
@@ -1,22 +1,35 @@
 package servletPackage;
 
+import javax.annotation.security.DeclareRoles;
+import javax.annotation.security.RolesAllowed;
 import javax.servlet.ServletContext;
-import javax.servlet.ServletContextEvent;
-import javax.servlet.ServletContextListener;
+import javax.servlet.ServletContextAttributeEvent;
+import javax.servlet.ServletContextAttributeListener;
 
-public class WebContext implements ServletContextListener {
+@DeclareRoles(value={"admin", "guest"})
+public class WebContext implements ServletContextAttributeListener {
 
 	private ServletContext context = null;
 
 	@Override
-	public void contextDestroyed(ServletContextEvent arg0) {
+	@RolesAllowed({"admin", "guest"})
+	public void attributeAdded(ServletContextAttributeEvent arg0) {
 		context = arg0.getServletContext();
-
+		System.out.println(arg0.getValue() + " was created");
 	}
 
 	@Override
-	public void contextInitialized(ServletContextEvent arg0) {
+	@RolesAllowed("admin")
+	public void attributeRemoved(ServletContextAttributeEvent arg0) {
 		context = arg0.getServletContext();
+		System.out.println(arg0.getValue() + " was removed");
 	}
+
+	@Override
+	public void attributeReplaced(ServletContextAttributeEvent arg0) {
+		context = arg0.getServletContext();
+		System.out.println(arg0.getValue() + " was changed");
+	}
+	
 
 }
